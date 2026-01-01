2086d88c0582dcefc6ff0e103a75ea575432b7c7
umerfayyaz2 <ufayyaz19@gmail.com>
2026-01-01 21:12:08 +0500

    Add Expense model and ExpenseCategory enum

---
+++ Sources/ExpenseTrackerApp/Models/ExpenseCategory.swift
@@ -0,0 +1,13 @@
+/// Defines all supported expense categories
+import Foundation
+
+enum ExpenseCategory: String, CaseIterable {
+
+    case food = "Food"
+    case transport = "Transport"
+    case shopping = "Shopping"
+    case bills = "Bills"
+    case health = "Health"
+    case entertainment = "Entertainment"
+    case other = "Other"
+}
